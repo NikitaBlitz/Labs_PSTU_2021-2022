c39a8a8e16237282e415db41c50faba96e2971d2
NikitaLekomtsev <90478242+NikitaBlitz@users.noreply.github.com>
2022-02-15 15:04:16 +0500

    Add files via upload

---
+++ 7.2/7.2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stdarg.h>
+using namespace std;
+
+int nok(int n,  ...)
+{
+	int sum= 1;
+	va_list param;
+	va_start(param, n);
+	for (int i = 0; i < n; i++)
+	{
+		sum *= va_arg(param, int);
+	}
+	va_end(param);
+	va_start(param, n);
+	int nod = va_arg(param, int);
+	for (int i=0; i <(n-1); i++)
+	{
+		int b = va_arg(param, int);
+		while (nod != 0 && b != 0)
+		{
+			if (nod > b) { nod = nod % b; }
+			else { b = b % nod; }
+		}
+		nod = (nod + b);
+	}
+	return (sum/nod);
+}
+
+
+
+int main()
+{
+	cout << nok(3, 6, 12, 24)<<endl;
+	cout << nok(5, 6, 14, 22,11,32)<< endl;
+	cout << nok(6, 43, 14, 24,53,21,78)<< endl;
+}
