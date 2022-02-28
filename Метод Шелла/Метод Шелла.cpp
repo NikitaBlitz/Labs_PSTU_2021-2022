ab27f66015e5ce2fa2c4e98fab448582d6a6c019
NikitaLekomtsev <90478242+NikitaBlitz@users.noreply.github.com>
2022-02-28 23:52:28 +0500

    Add files via upload

---
+++ Метод Шелла/Метод Шелла.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+
+
+int main()
+{
+	setlocale(LC_ALL, "Ru");
+	srand(time(NULL));
+	int n, d, b;
+	cout << "Введите размер массива: ";
+	cin >> n;
+	int* A = new int[n];
+	for (int i = 0; i < n; i++)
+	{
+		A[i] = rand() % 100;
+	}
+	cout << "Изначальный массив: ";
+	for (int i = 0; i < n; i++)
+	{
+		cout << A[i] << " ";
+	}
+	cout << endl;
+	cout << "Итоговый массив: ";
+	d = n;
+	d = d / 2;
+	while (d > 0)
+	{
+		for (int i = 0; i < n - d; i++)
+		{
+			int j = i;
+			while (j >= 0 && A[j] > A[j + d])
+			{
+				b = A[j];
+				A[j] = A[j + d];
+				A[j + d] = b;
+				j--;
+			}
+		}
+		d = d / 2;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		cout << A[i] << " ";
+	}
+	delete[] A;
+	return 0;
+}
