dd8509582d4029ab9a6dac36f4408e26322891b6
NikitaLekomtsev <90478242+NikitaBlitz@users.noreply.github.com>
2022-02-28 21:46:33 +0500

    Add files via upload

---
+++ Ханойская башня/Ханойская башня.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int Hoop(int n, int start, int temp, int point)
+{
+	if (n > 0)
+	{
+		Hoop(n - 1, start, point, temp);
+		cout << start << " => " << temp << endl;
+		Hoop(n - 1, point, temp, start);
+	}
+	return 0;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int n;
+	cout << "Введите количество обручей: ";
+	cin >> n;
+	Hoop(n, 1, 3, 2);
+	return 0;
+}
