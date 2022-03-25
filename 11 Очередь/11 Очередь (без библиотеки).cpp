27cee7ff5dfe2e0fce6cd67a1f5e380be6f78984
NikitaLekomtsev <90478242+NikitaBlitz@users.noreply.github.com>
2022-03-26 01:57:30 +0500

    Add files via upload

---
+++ 11 Очередь/11 Очередь (без библиотеки).cpp
@@ -0,0 +1,127 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template <typename T>
+struct Node
+{
+	T data;
+	Node<T>* next;
+};
+
+template <typename T>
+struct Queue
+{
+	int size = 0;
+	Node<T>* head = nullptr;
+	Node<T>* tail = nullptr;
+};
+
+template <typename T>
+void newQueue(Queue<T>& q, const int& n)
+{
+	T a;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a;
+		push(q, a);
+	}
+}
+
+template <typename T>
+void push(Queue<T>& q, const T& data)
+{
+	if (q.head != nullptr)
+	{
+		Node<T>* newnode = new Node<T>;
+		q.size++;
+		newnode->data = data;
+		newnode->next = nullptr;
+		q.tail->next = newnode;
+		q.tail = newnode;
+	}
+	else
+	{
+		Node<T>* newnode = new Node<T>;
+		newnode->data = data;
+		q.head = newnode;
+		q.tail = newnode;
+		q.size = 1;
+	}
+}
+
+template <typename T>
+void pop(Queue<T>& q)
+{
+	Node<T>* tmp = q.head;
+	q.head = q.head->next;
+	q.size--;
+	delete tmp;
+}
+
+template <typename T>
+void print(Queue<T>& q)
+{
+	Node<T>* tmp = q.head;
+	while (tmp != nullptr)
+	{
+		cout << tmp->data;
+		tmp = tmp->next;
+	}
+}
+
+template <typename T>
+void change(Queue<T>& q, int& n, int& k)
+{
+	int count = 1;
+	for (int i=0; i<n; i++)
+	{
+		if (k==count)
+		{
+			cout << "Введите новый элемент: ";
+			T a;
+			cin >> a;
+			push(q, a);
+			pop(q);
+		}
+		else
+		{
+			push(q, q.head->data);
+			pop(q);
+		}
+		count++;
+	}
+}
+
+template <typename T>
+void deleteQ(Queue<T>& q)
+{
+	while (q.head != nullptr)
+	{
+		pop(q);
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n,k;
+	char a;
+	cout << "Введите размер очереди: ";
+	cin >> n;
+	Queue<char> queue;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a;
+		push(queue, a);
+	}
+	print(queue);
+	cout << endl;
+	cout << "Введите номер элемента очереди: ";
+	cin >> k;
+	change(queue, n, k);
+	cout << endl;
+	print(queue);
+	deleteQ(queue);
+	return 0;
+}
---
+++ 11 Очередь/11 Очередь (библиотека).cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <queue>
+
+using namespace std;
+
+void print(queue<char> q)
+{
+	int num = q.size();
+	for (int i = 0; i < num; i++)
+	{
+		cout << q.front();
+		q.pop();
+	}
+}
+
+void change(queue<char>& q, int& size, int& n)
+{
+	int count = 1;
+	for (int i=0; i<size;i++)
+	{
+		if (count==n)
+		{
+			cout << "Введите новый элемент очереди: ";
+			char a;
+			cin >> a;
+			q.push(a);
+			q.pop();
+		}
+		else
+		{
+			q.push(q.front());
+			q.pop();
+		}
+		count++;
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n,k;
+	char a;
+	cout << "Введите размер очереди: ";
+	cin >> n;
+	cout << "Введите элемент очереди, который хотите поменять: ";
+	cin >> k;
+	queue<char> qu;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a;
+		qu.push(a);
+	}
+	cout << "Начальная очередь: ";
+	print(qu);
+	cout << endl;
+	change(qu, n, k);
+	cout << endl;
+	cout << "Финальная очередь: ";
+	print(qu);
+	return 0;
+}
