2b4f56040aa500eb671cb063db7c05ad189918b3
NikitaLekomtsev <90478242+NikitaBlitz@users.noreply.github.com>
2022-03-26 01:56:09 +0500

    Add files via upload

---
+++ 11 Стек/11 Стек (без библиотеки).cpp
@@ -0,0 +1,89 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template <typename T>
+struct Node
+{
+	T data;
+	Node<T>* next;
+};
+
+template <typename T>
+struct Stack
+{
+	Node<T>* head = nullptr;
+	int size = 0;
+};
+
+template <typename T>
+void setNode(Node<T>& node, T data, Node<T>* next = null)
+{
+	node = new Node<T>;
+	node->next = next;
+	node->data = data;
+}
+
+template <typename T>
+void Push(Stack<T>& tmp, const T& data)
+{
+	Node<T>* newnode = new Node<T>;
+	newnode->next = tmp.head;
+	newnode->data = data;
+	tmp.head = newnode;
+	tmp.size++;
+}
+
+template <typename T>
+void pop(Stack<T>& tmp)
+{
+	if (tmp.head != nullptr)
+	{
+		Node<T>* deleted = tmp.head;
+		tmp.head = tmp.head->next;
+		delete deleted;
+		tmp.size--;
+	}
+}
+
+template <typename T>
+void printStack(Stack<T>& tmp)
+{
+	Node<T>* currentnode = tmp.head;
+	while (currentnode != nullptr)
+	{
+		cout << currentnode->data << " ";
+		currentnode = currentnode->next;
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n;
+	int count = 1;
+	cout << "Введите размер стака: ";
+	cin >> n;
+	double k;
+	Stack <double> stack1;
+	Stack <double> stack2;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> k;
+		Push(stack1, k);
+	}
+	cout << endl;
+	printStack(stack1);
+	cout << endl;
+	for (int i = 0; i < n; i++)
+	{
+		if (count % 2 != 0)
+		{
+			Push(stack2, stack1.head->data);
+		}
+		pop(stack1);
+		count++;
+	}
+	printStack(stack2);
+	return 0;
+}
---
+++ 11 Стек/11 Стек (библиотека).cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <stack>
+
+using namespace std;
+
+void print(stack<double> st)
+{
+	int num = st.size();
+	for (int i = 0; i < num; i++)
+	{
+		cout << st.top() << " ";
+		st.pop();
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int size;
+	double a;
+	cout << "Введите размера стека: ";
+	cin >> size;
+	stack<double> stack1;
+	for (int i = 0; i < size; i++)
+	{
+		cin >> a;
+		stack1.push(a);
+	}
+	print(stack1);
+	cout << endl;
+	stack<double> stack2;
+	int count = 1;
+	for (int i = 0; i < size; i++)
+	{
+		if (count % 2 == 0)
+		{
+			stack2.push(stack1.top());
+		}
+		stack1.pop();
+		count++;
+	}
+	int size2 = stack2.size();
+	for (int i = 0; i < size2; i++)
+	{
+		cout << stack2.top() << " ";
+		stack2.pop();
+	}
+	print(stack2);
+}
